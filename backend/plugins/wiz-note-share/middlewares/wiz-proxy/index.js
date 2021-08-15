48d38705aa7cb10ae4dd6eee0f99b2e253429586
N2ET <n2et@qq.com>
2021-08-15 15:51:48 +0800

    feat: 添加缓存功能，未开启
    测试：
    1. 缓存存在问题

--- backend/plugins/wiz-note-share/middlewares/wiz-proxy/index.js
+++ backend/plugins/wiz-note-share/middlewares/wiz-proxy/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
 const proxy = require('koa-proxies');
 
 // const util = require('./util');
@@ -16,20 +18,101 @@ module.exports = strapi => ({
             return;
         }
 
+        const catchDir = path.join(
+            __dirname,
+            'static'
+        );
+
+        if (!fs.existsSync(catchDir)) {
+            fs.mkdirSync(catchDir);
+        }
+
+
+        function isCacheable (url) {
+            
+            return false;
+
+            // return /\.js$/.test(url);
+        }
+
+        function getFilePath (url) {
+            let targetFile = url.replace(/\//g, '_');
+            targetFile = path.join(
+                catchDir,
+                targetFile
+            );
+            return targetFile;
+        }
+
+        function getCacheFile (url) {
+
+            let targetFile = getFilePath(url);
+
+            if (fs.existsSync(targetFile)) {
+
+                return fs.readFileSync(
+                    targetFile,
+                    {
+                        encoding: 'utf-8'
+                    }
+                );
+
+                // return fs.createReadStream(targetFile);
+            }
+        }
+
+        function cacheFile (url, res) {
+            
+            let file = getFilePath(url);
+
+            const stream = fs.createWriteStream(file, {
+                encoding: 'utf-8'
+            });
+
+            res.pipe(stream);
+        }
+
         config.proxyPaths.forEach(item => {
             strapi.app.use(
                 proxy(item, {
                     target: config.server,
                     changeOrigin: true,
                     logs: true,
-                    // events: {
-                    //     proxyRes (proxyRes, req, res) {
-                    //         
-                    //     },
-                    //     error (err, req, res) {
-                    //        
-                    //     }
-                    // }
+                    events: {
+                        proxyReq (proxyReq, req, res) {
+
+                            const url = req.url;
+                            if (isCacheable(url)) {
+
+                                let stream = getCacheFile(url);  
+                                if (stream) {
+                                    // stream.pipe(res);
+                                    res.body = stream;
+                                    // res.end();
+                                }
+
+                            }
+
+                        },
+
+                        proxyRes (proxyRes, req, res) {
+                            
+                            const url = req.url;
+                            if (isCacheable(url)) {
+                                if (!fs.existsSync(
+                                    getFilePath(url)
+                                )) {
+                                    cacheFile(url, proxyRes);
+                                }
+                            }
+
+                        },
+
+
+                        // error (err, req, res) {
+                           
+                        // }
+                    }
                 })
             );
         });
